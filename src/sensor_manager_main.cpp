f142d5a1577310c06d17767fb8e93c40fa8ad85f
taewook <utsi09@naver.com>
2025-03-29 14:06:57 +0900

    테스트수정

--- src/sensor_manager_main.cpp
+++ src/sensor_manager_main.cpp
@@ -13,7 +13,7 @@
 #include "bt_sensor_manager/image_alive_condition.hpp"
 
 using namespace std::chrono_literals;
-//branch test 테스트 브랜치 테스트2-commit
+//branch test 테스트 브랜치 테스트2-commit 2에서수정
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
